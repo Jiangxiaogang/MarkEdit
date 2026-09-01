f9700c3ff2568068715439b3db3d76800793b007
jiangxiaogang <kerndev@foxmail.com>
2026-09-01 16:54:05 +0800

    .

--- src/previewwidget.cpp
+++ src/previewwidget.cpp
@@ -14,7 +14,7 @@ PreviewWidget::PreviewWidget(QWidget *parent)
     : QWebView(parent)
     , m_parser(new MarkdownParser(this))
     , m_loader(new StyleSheetLoader(this))
-    , m_css(StyleSheetLoader::getDefaultCSS())
+    , m_css("")
     , m_emitScroll(true)
     , m_lastScroll(0)
     , m_pendingRatio(-1.0f)
@@ -25,17 +25,12 @@ PreviewWidget::PreviewWidget(QWidget *parent)
     m_scrollTimer = new QTimer(this);
     m_scrollTimer->setInterval(100);
     connect(m_scrollTimer, SIGNAL(timeout()), this, SLOT(onScrollTimeout()));
-    // Poll the scroll position so user scrolling in the preview is reported
-    // via the `scrolled` signal and synced to the editor.
     m_scrollTimer->start();
 
     connect(this, SIGNAL(linkClicked(const QUrl &)), this, SLOT(onLinkClicked(const QUrl &)));
     connect(this, SIGNAL(loadFinished(bool)), this, SLOT(onLoadFinished(bool)));
-
-    // Apply the configured browser font families to the preview.
     applyFontSettings();
-    connect(ConfigManager::instance(), SIGNAL(configurationChanged()),
-            this, SLOT(applyFontSettings()));
+    connect(ConfigManager::instance(), SIGNAL(configurationChanged()), this, SLOT(applyFontSettings()));
 }
 
 PreviewWidget::~PreviewWidget()
@@ -58,9 +53,6 @@ void PreviewWidget::refresh()
 {
     QString body = m_parser->parse(m_markdown);
     QString html = generateHtml(body);
-    // Suppress scroll reporting while the new content loads asynchronously,
-    // otherwise the transient scroll position during reflow would be synced
-    // back to the editor.
     m_emitScroll = false;
     setHtml(html, m_baseUrl);
     m_lastScroll = 0;
@@ -85,8 +77,8 @@ void PreviewWidget::applyFontSettings()
 QString PreviewWidget::generateHtml(const QString &body) const
 {
     return QString(
-               "<!DOCTYPE html><html><head><meta charset=\"utf-8\">"
-               "<style>%1</style></head><body>%2</body></html>")
+               "<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"utf-8\">\n"
+               "<style>%1</style>\n</head>\n<body>\n%2\n</body>\n</html>")
            .arg(m_css)
            .arg(body);
 }
@@ -126,11 +118,8 @@ void PreviewWidget::applyRatio(float ratio)
 void PreviewWidget::onLoadFinished(bool ok)
 {
     Q_UNUSED(ok);
-    // setHtml() is asynchronous: re-apply the requested scroll position
-    // once the new content has finished loading.
     if (m_pendingRatio >= 0.0f)
         applyRatio(m_pendingRatio);
-    // Content has settled; resume reporting scroll changes to sync the editor.
     m_emitScroll = true;
 }
 
@@ -148,8 +137,6 @@ void PreviewWidget::onScrollTimeout()
 
 void PreviewWidget::onLinkClicked(const QUrl &url)
 {
-    // Open external / local links in the default browser instead of navigating
-    // inside the preview pane.
     if (url.scheme().startsWith("http") || url.scheme().startsWith("file"))
     {
         QDesktopServices::openUrl(url);
