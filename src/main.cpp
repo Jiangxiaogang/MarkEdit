e1057ffd2b9786952746cf2394c6d8dafab036c9
jiangxiaogang <kerndev@foxmail.com>
2026-09-02 10:38:26 +0800

    解决弹出右键菜单后，选中文字颜色变灰

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,11 @@ int main(int argc, char *argv[])
     QApplication::setApplicationName("MarkEdit");
     QApplication::setApplicationVersion("1.0.0");
 
+    QPalette globalPal = qApp->palette();
+    globalPal.setColor(QPalette::Inactive, QPalette::Highlight,globalPal.color(QPalette::Active, QPalette::Highlight));
+    globalPal.setColor(QPalette::Inactive, QPalette::HighlightedText,globalPal.color(QPalette::Active, QPalette::HighlightedText));
+    qApp->setPalette(globalPal);
+
     QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
     QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
     QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
